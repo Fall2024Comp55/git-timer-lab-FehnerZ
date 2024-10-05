bfae558fde6da1e7da0b9bed2665aecaea5674f2
zacfe <z_fehner@u.pacific.edu>
2024-10-05 14:56:06 -0700

    timing mechanism, an event listener/callback, and control functions

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,20 +1,45 @@
 import acm.graphics.*;
 import acm.program.*;
-
-public class MyFirstTimer extends GraphicsProgram {
+import javax.swing.*;
+import java.awt.event.*;
+public class MyFirstTimer extends GraphicsProgram implements ActionListener {
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
+	Timer someTimerVar = new Timer(1000, this);
 	private GLabel myLabel;
+	private int numTimes = 0;
+	//private TimerBall ball;
+	
 
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
-		requestFocus();
+		requestFocus(); 
 	}
 	
 	public void run() {
 		myLabel = new GLabel("# of times called?", 0, 100);
 		add(myLabel);
+		setInitialDelay(3);
+		someTimerVar.start();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		numTimes++;
+		myLabel.move(5, 0);
+		myLabel.setLabel("times called? " + numTimes);
+		stopTimer();
+	}
+	
+	public void setInitialDelay(int i) {
+		int j = i;
+	}
+	
+	public void stopTimer() {
+		if (numTimes == 10) {
+			someTimerVar.stop();
+		}
 	}
 	
 	public static void main(String[] args) {
